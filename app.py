cfb42c5f0c7f975b114b34c1d1bbda073eaf55bf
nerdsolomon <nerdsolomon@gmail.com>
2024-03-11 03:25:02 +0400

    o

--- app.py
+++ app.py
@@ -436,7 +436,8 @@ def cbt_question(id):
     		else:
     			cbt = CBT(question=form.question.data, answer=form.answer.data, option1=form.option1.data, option2=form.option2.data, option3=form.option3.data, subject_id=id, type=request.form['type'])
     			store(cbt)
-    		flash("Question added successfully.")
+            flash("Question added successfully.")
+            
         elif form_type == "subject_name":
             subject.title = form.answer.data
             store(subject)
