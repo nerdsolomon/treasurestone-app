ba1e154e933c55dfa9e46cae3d98ff57cf9bb2d7
nerdsolomon <nerdsolomon@gmail.com>
2024-03-11 03:35:31 +0400

    o

--- app.py
+++ app.py
@@ -435,15 +435,15 @@ def cbt_question(id):
     				flash("Invalid file type. Allowed types: {}".format(", ".join(ALLOWED_EXTENSIONS)))
     		else:
     			cbt = CBT(question=form.question.data, answer=form.answer.data, option1=form.option1.data, option2=form.option2.data, option3=form.option3.data, subject_id=id, type=request.form['type'])
-                store(cbt)
-            flash("Question added successfully.")
-
+    			store(cbt)
+    		flash("Question added successfully.")
+    	
         elif form_type == "subject_name":
             subject.title = form.answer.data
             store(subject)
             flash("Subject Edited Successfully.")
-
-        return redirect(url_for('cbt_question', id=id))
+    	
+    	return redirect(url_for('cbt_question', id=id))
     return render_template('cbt_question.html', subject=subject, cbts=cbts, form=form)
     
 
